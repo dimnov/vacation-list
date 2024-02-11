c36303e71eda0d6f1df80aac9967d1a120add025
dimnov <dimnov1512@gmail.com>
2024-02-11 23:07:27 +0200

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Form from "./components/Form.jsx";
+import Logo from "./components/Logo.jsx";
+import PackingList from "./components/PackingList.jsx";
+import Stats from "./components/Stats.jsx";
+
+export default function App() {
+  return (
+    <div className="app">
+    </div>
+  );
+}
