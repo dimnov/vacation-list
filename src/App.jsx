e3c966f11876bbd6030cbe52b55ca5bc21e87fb1
dimnov <dimnov1512@gmail.com>
2024-04-14 21:23:48 +0300

    Add delete and add item functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,25 @@ import Form from "./components/Form.jsx";
 import Logo from "./components/Logo.jsx";
 import PackingList from "./components/PackingList.jsx";
 import Stats from "./components/Stats.jsx";
+import { useState } from "react";
 
 export default function App() {
-  return (
-    <div className="app">
-      <Logo />
-      <Form />
-      <PackingList />
-      <Stats />
-    </div>
-  );
+ const [items, setItems] = useState([]);
+
+ function handleAddItems(item) {
+  setItems((items) => [...items, item]);
+ }
+
+ function handleDeleteItem(itemId) {
+  setItems((items) => items.filter((item) => item.id !== itemId));
+ }
+
+ return (
+  <div className="app">
+   <Logo />
+   <Form onAddItems={handleAddItems} />
+   <PackingList onDeleteItem={handleDeleteItem} items={items} />
+   <Stats />
+  </div>
+ );
 }
--- src/components/PackingList.jsx
+++ src/components/PackingList.jsx
@@ -1,19 +1,13 @@
 import Item from "./Item.jsx";
 
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 3, description: "Charger", quantity: 1, packed: true },
-];
-
-export default function PackingList() {
-  return (
-    <div className="list">
-      <ul>
-        {initialItems.map((item) => (
-          <Item item={item} key={item.id}></Item>
-        ))}
-      </ul>
-    </div>
-  );
+export default function PackingList({ items, onDeleteItem }) {
+ return (
+  <div className="list">
+   <ul>
+    {items.map((item) => (
+     <Item item={item} key={item.id} onDeleteItem={onDeleteItem}></Item>
+    ))}
+   </ul>
+  </div>
+ );
 }
